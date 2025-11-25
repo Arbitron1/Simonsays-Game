18116d6d54fd5ccc455445c5b441867c7ac8de0f
Arbitron1 <kambleaum1802@gmail.com>
2025-11-25 19:57:05 +0530

    Fix Simon Says colors, random index bug, clean code, rename files

--- script.js
+++ script.js
@@ -7,10 +7,12 @@ let started = false;
 let level = 0;
 
 let h2 = document.querySelector("h2");
+let info = document.getElementById("info");
 
 document.addEventListener("keypress", function () {
   if (!started) {
     started = true;
+    info.innerText = "Watch the pattern carefully!";
     levelUp();
   }
 });
@@ -30,7 +32,7 @@ function levelUp() {
   level++;
   h2.innerText = `Level ${level}`;
 
-  let randIdx = Math.floor(Math.random() * 4); 
+  let randIdx = Math.floor(Math.random() * 4);
   let randColor = btns[randIdx];
   let randBtn = document.querySelector(`#${randColor}`);
 
@@ -40,16 +42,16 @@ function levelUp() {
 
 function checkAns(idx) {
   if (userSeq[idx] === gameSeq[idx]) {
-
     if (userSeq.length === gameSeq.length) {
+      info.innerText = "Watch the next pattern!";
       setTimeout(levelUp, 1000);
     }
-
   } else {
-    h2.innerHTML = `Game Over! Your score was <b>${level}</b>  <br> Press any key to restart`;
+    h2.innerHTML = `Game Over! Your score was <b>${level}</b><br>Press any key to restart.`;
+    info.innerText = "Press any key to start again.";
 
     document.body.style.backgroundColor = "red";
-    setTimeout(() => document.body.style.backgroundColor = "white", 200);
+    setTimeout(() => (document.body.style.backgroundColor = "white"), 200);
 
     reset();
   }
@@ -61,6 +63,8 @@ function btnPress() {
 
   let userColor = btn.getAttribute("id");
   userSeq.push(userColor);
+
+  info.innerText = "Your turn! Repeat the pattern.";
 
   checkAns(userSeq.length - 1);
 }
